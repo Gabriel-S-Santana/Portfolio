301a88e69a5b92415d50740c5279803153e10ec1
Gabriel-S-Santana <gabrielsouzasantana2@gmail.com>
2024-06-15 17:44:34 -0300

    add: navbar-mobile

--- src/app/sections/navbar/navbar.component.ts
+++ src/app/sections/navbar/navbar.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
+import * as feather from 'feather-icons';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'gs-navbar',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgClass, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+    isMobile: boolean = false;
+    isGone: boolean = false;
 
+    ngAfterViewInit() {
+        feather.replace();
+    }
+
+    navToggle(){
+        if(this.isMobile == true) {
+            this.isGone = false
+            setTimeout(() => {
+                this.isMobile = false
+            }, 500);
+        }
+        else {
+            this.isGone = true
+            this.isMobile = true
+        }
+    }
 }
